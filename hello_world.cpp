162889410055b79322d1520ce26c0ce3c7368211
MartinWenge <martin.wengenmayr@mailbox.tu-dresden.de>
2015-12-07 15:54:57 +0100

    Update hello_world.cpp
    Adding command line parsing option to hello world.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 //This file is used as a feature for the example repo hello world
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[] ){
   std::cout << "Hello World!\n";
   return 0;
 }
